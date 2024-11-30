e4245833ffa70c04eeaada762590101d3fd5c767
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-11-30 16:52:29 -0300

    fix: :bug: update UserSerializer meta class to map only needed fields

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -2,9 +2,22 @@ from rest_framework import serializers
 from .models import User
 
 
+# TODO: expecify model fields to be mapped by ModelSerializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = [
+            "id",
+            "username",
+            "password",
+            "email",
+            "first_name",
+            "last_name",
+            "is_superuser",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
 
     def create(self, validated_data: dict) -> User:
         return User.objects.create_user(**validated_data)
