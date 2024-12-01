31f12c586dec6a97878bb6853c4a48b9eaedb50c
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-01 19:58:49 -0300

    chore: :coffin: remove todo comment on user serializer

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import User
 
 
-# TODO: expecify model fields to be mapped by ModelSerializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
