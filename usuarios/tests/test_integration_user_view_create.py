ebfbeb533ffa7ac5dbcfd7636a1405d2707aa067
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-01 23:51:37 -0300

    test: :test_tube: add user create view integration failing test for required fields

--- usuarios/tests/test_integration_user_view_create.py
+++ usuarios/tests/test_integration_user_view_create.py
@@ -38,3 +38,23 @@ class UserViewIntegrationTest(APITestCase):
         resulted_response_data = response.json()
 
         assert expected_response_data == resulted_response_data
+
+    def test_user_creation_with_missing_required_fields(self):
+        user_data = {}
+
+        response = self.client.post(self.BASE_URL, data=user_data, format="json")
+
+        expected_status_code = status.HTTP_400_BAD_REQUEST
+        resulted_status_code = response.status_code
+        assert expected_status_code == resulted_status_code
+
+        expected_response_data = {
+            "username": ["This field is required."],
+            "password": ["This field is required."],
+            "first_name": ["This field is required."],
+            "last_name": ["This field is required."],
+            "is_superuser": ["This field is required."],
+        }
+        resulted_response_data = response.json()
+
+        assert expected_response_data == resulted_response_data
