a794c5199674fdada6e5c49ce03b11170595fbfc
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-02 01:09:36 -0300

    fix: :bug: update custom auth user model to have unique email validation with custom error message

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,5 +5,9 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(
+        max_length=127,
+        unique=True,
+        error_messages={"unique": "A user with that email already exists."},
+    )
     is_superuser = models.BooleanField()
