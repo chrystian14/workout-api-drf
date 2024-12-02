d775db94d162b7d8b988c2c241da8abac088dd9e
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-01 22:52:57 -0300

    test: :recycle: refactor user creation with valid data test scope to not use with check context

--- usuarios/tests/test_integration_user_view_create.py
+++ usuarios/tests/test_integration_user_view_create.py
@@ -1,6 +1,5 @@
 from rest_framework.test import APITestCase
 from rest_framework.views import status
-from pytest_check import check
 
 
 class UserViewIntegrationTest(APITestCase):
@@ -20,19 +19,17 @@ class UserViewIntegrationTest(APITestCase):
 
         response = self.client.post(self.BASE_URL, data=user_data, format="json")
 
-        with check:
-            expected_status_code = status.HTTP_201_CREATED
-            resulted_status_code = response.status_code
-            self.assertEqual(expected_status_code, resulted_status_code)
+        expected_status_code = status.HTTP_201_CREATED
+        resulted_status_code = response.status_code
+        self.assertEqual(expected_status_code, resulted_status_code)
 
-        with check:
-            expected_response_data = {
-                "id": 1,
-                "username": user_data["username"],
-                "email": user_data["email"],
-                "first_name": user_data["first_name"],
-                "last_name": user_data["last_name"],
-                "is_superuser": user_data["is_superuser"],
-            }
-            resulted_response_data = response.json()
-            self.assertEqual(expected_response_data, resulted_response_data)
+        expected_response_data = {
+            "id": 1,
+            "username": user_data["username"],
+            "email": user_data["email"],
+            "first_name": user_data["first_name"],
+            "last_name": user_data["last_name"],
+            "is_superuser": user_data["is_superuser"],
+        }
+        resulted_response_data = response.json()
+        self.assertEqual(expected_response_data, resulted_response_data)
