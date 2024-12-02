eff1a6ff988cb7843f27a4b4fe39576ce6a6cb10
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-02 01:58:09 -0300

    test: :white_check_mark: add user login view integration test for valid credencials

---
+++ usuarios/tests/test_login.py
@@ -0,0 +1,34 @@
+from rest_framework.test import APITestCase
+from rest_framework.views import status
+import pytest
+
+from usuarios.models import User
+
+
+@pytest.mark.describe("POST /api/login")
+class LoginTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.BASE_URL = "/api/login"
+
+        cls.user_data = {
+            "username": "john2099",
+            "password": "my-very-secret-password2099#",
+            "email": "john-doe@mail.com.br",
+            "first_name": "John",
+            "last_name": "Doe",
+            "is_superuser": False,
+        }
+
+        cls.created_user = User.objects.create_user(**cls.user_data)
+
+    def test_login_with_valid_credencials(self):
+        user_credentials = {
+            "username": self.user_data["username"],
+            "password": self.user_data["password"],
+        }
+        response = self.client.post(self.BASE_URL, data=user_credentials, format="json")
+
+        expected_response_status_code = status.HTTP_200_OK
+        resulted_response_status_code = response.status_code
+        assert resulted_response_status_code == expected_response_status_code
