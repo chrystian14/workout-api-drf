31e24688fc936f4b0fad1f5d5d7c246bc2316156
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-02 16:29:01 -0300

    test: :test_tube: add grupo muscular create view failing test for non admin token

--- grupos_musculares/tests/test_integration_grupo_muscular_view_create.py
+++ grupos_musculares/tests/test_integration_grupo_muscular_view_create.py
@@ -23,11 +23,40 @@ class GrupoMuscularViewIntegrationTest(APITestCase):
         cls.super_user = User.objects.create_superuser(**super_user_data)
         cls.super_user_access_token = str(AccessToken.for_user(cls.super_user))
 
+        regular_user_data = {
+            "username": "regular-user",
+            "password": "my_secret_password",
+            "email": "regular-user@mail.com.br",
+            "first_name": "John",
+            "last_name": "Doe",
+        }
+
+        cls.regular_user = User.objects.create_user(**regular_user_data)
+        cls.regular_user_access_token = str(AccessToken.for_user(cls.regular_user))
+
     def test_grupo_muscular_creation_without_token(self):
         grupo_muscular_data = {
             "nome": "Cardio",
             "descricao": "Exercícios de cardiovascular",
         }
+
+        response = self.client.post(
+            self.BASE_URL, data=grupo_muscular_data, format="json"
+        )
+
+        expected_status_code = status.HTTP_401_UNAUTHORIZED
+        resulted_status_code = response.status_code
+        assert resulted_status_code == expected_status_code
+
+    def test_grupo_muscular_creation_with_non_admin_token(self):
+        grupo_muscular_data = {
+            "nome": "Cardio",
+            "descricao": "Exercícios de cardiovascular",
+        }
+
+        self.client.credentials(  # type: ignore
+            HTTP_AUTHORIZATION="Bearer " + self.super_user_access_token
+        )
 
         response = self.client.post(
             self.BASE_URL, data=grupo_muscular_data, format="json"
