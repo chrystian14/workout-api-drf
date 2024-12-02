d15aa87bafbc1136127f22bfbd40dd803ff7ea10
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-02 18:48:32 -0300

    fix: :bug: prevent non admin users from create exercicios

--- exercicios/views.py
+++ exercicios/views.py
@@ -1,8 +1,14 @@
 from rest_framework import generics
 from .models import Exercicio
 from .serializers import ExercicioSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from usuarios.permissions import IsAdmin
 
 
 class ExercicioView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdmin]
+
     queryset = Exercicio.objects.all()
     serializer_class = ExercicioSerializer
