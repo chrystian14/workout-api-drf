87ff90989948c8ae208403f9237026e58b9c769e
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-02 17:40:44 -0300

    fix: :bug: prevent non admin users from create and list grupo muscular

--- grupos_musculares/views.py
+++ grupos_musculares/views.py
@@ -3,11 +3,12 @@ from .models import GrupoMuscular
 from .serializers import GrupoMuscularSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from usuarios.permissions import IsAdmin
 
 
 class GrupoMuscularView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdmin]
 
     queryset = GrupoMuscular.objects.all()
     serializer_class = GrupoMuscularSerializer
