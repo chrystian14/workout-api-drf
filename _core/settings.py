b7e756612eb1ba498a2e1311f220a0a58f272966
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-02 01:30:54 -0300

    feat: :sparkles: add simplejwt package and login and refresh routes with expiration times

--- _core/settings.py
+++ _core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -110,6 +111,12 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+# djangorestframework-simplejwt configs
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+}
 
 
 # Internationalization
