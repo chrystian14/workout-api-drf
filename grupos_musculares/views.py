0b7cf2e7d72440a5e18755f83f2e1767a2867a79
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-02 16:20:31 -0300

    fix: :bug: prevent grupo muscular create view to be access by unauthorized users

--- grupos_musculares/views.py
+++ grupos_musculares/views.py
@@ -1,8 +1,13 @@
 from rest_framework import generics
 from .models import GrupoMuscular
 from .serializers import GrupoMuscularSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class GrupoMuscularView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = GrupoMuscular.objects.all()
     serializer_class = GrupoMuscularSerializer
