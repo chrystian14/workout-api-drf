9c54b96c10ae9493411456e1e92148cccf862783
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-02 13:24:45 -0300

    feat: :sparkles: add Exercicio and GrupoMuscular models with 1-N relationship

--- exercicios/models.py
+++ exercicios/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+# Todo: Pesquisar fichas de treino de academias
+class Exercicio(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField(blank=True, null=True)
+
+    grupo_muscular = models.ForeignKey(
+        "grupos_musculares.GrupoMuscular",
+        related_name="exercicios",
+        on_delete=models.CASCADE,
+    )
--- grupos_musculares/models.py
+++ grupos_musculares/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class GrupoMuscular(models.Model):
+    nome = models.CharField(max_length=127, unique=True)
+    descricao = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
