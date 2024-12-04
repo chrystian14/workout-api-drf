ef5d4223f595f5b73a9eb8c990e320173fdfffea
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-04 17:20:51 -0300

    chore: :coffin: remove unused import

--- grupos_musculares/urls.py
+++ grupos_musculares/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import GrupoMuscularView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("grupos-musculares", GrupoMuscularView.as_view()),
