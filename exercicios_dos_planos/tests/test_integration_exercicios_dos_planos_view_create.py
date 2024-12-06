49ccd44a90f6978229f67217755668d0a00dab9d
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-12-05 21:41:18 -0300

    chore: :truck: rename exercicios dos planos tests

--- exercicios_dos_planos/tests/test_integration_exercicios_dos_planos_view_create.py
+++ exercicios_dos_planos/tests/test_integration_exercicios_dos_planos_view_create.py
@@ -1,12 +1,10 @@
+import pytest
+import factory
+
+from usuarios.factories import AdminUserFactory, RegularUserFactory
+from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework.test import APITestCase
 from rest_framework.views import status
-from rest_framework_simplejwt.tokens import AccessToken
-import pytest
-from planos_de_treino.factories import PlanoDeTreinoFactory
-from usuarios.factories import AdminUserFactory, RegularUserFactory
-import factory
-from unittest.mock import patch
-from django.utils import timezone
 
 
 @pytest.mark.describe("POST /api/exercicios-dos-planos")
@@ -28,7 +26,7 @@ class PlanoDeTreinoViewIntegrationTest(APITestCase):
         resulted_status_code = response.status_code
         assert resulted_status_code == expected_status_code
 
-    def test_plano_de_treino_creation_with_non_admin_token(self):
+    def test_exercicios_do_plano_creation_with_non_admin_token(self):
         self.client.credentials(  # type: ignore
             HTTP_AUTHORIZATION="Bearer " + self.regular_user_access_token
         )
@@ -39,7 +37,7 @@ class PlanoDeTreinoViewIntegrationTest(APITestCase):
         resulted_status_code = response.status_code
         assert resulted_status_code == expected_status_code
 
-    def test_plano_de_treino_creation_with_missing_required_fields(self):
+    def test_exercicios_do_plano_creation_with_missing_required_fields(self):
         plano_de_treino_data = {}
 
         self.client.credentials(  # type: ignore
@@ -61,30 +59,3 @@ class PlanoDeTreinoViewIntegrationTest(APITestCase):
         }
         resulted_response_data = response.json()
         assert resulted_response_data == expected_response_data
-
-    # @patch("django.utils.timezone.now", return_value=timezone.now())
-    # def test_plano_de_treino_creation_with_valid_data(self, mock_now):
-    #     plano_de_treino_data = factory.build(
-    #         dict, FACTORY_CLASS=PlanoDeTreinoFactory, usuario=self.regular_user
-    #     )
-
-    #     self.client.credentials(  # type: ignore
-    #         HTTP_AUTHORIZATION="Bearer " + self.super_user_access_token
-    #     )
-
-    #     response = self.client.post(
-    #         self.BASE_URL, data=plano_de_treino_data, format="json"
-    #     )
-
-    #     expected_status_code = status.HTTP_201_CREATED
-    #     resulted_status_code = response.status_code
-    #     assert resulted_status_code == expected_status_code
-
-    #     expected_response_data = {
-    #         "id": 1,
-    #         "nome": plano_de_treino_data["nome"],
-    #         "usuario": plano_de_treino_data["usuario"],
-    #         "data_criacao": mock_now.return_value.isoformat().replace("+00:00", "Z"),
-    #     }
-    #     resulted_response_data = response.json()
-    #     assert resulted_response_data == expected_response_data
